5a455e9964e8b0003bb2b5f05e21e2f6ef32f857
Maikon Schumann <maikon.schumann@gmail.com>
2024-06-12 16:02:13 +0000

    initial deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4w21nyj9(w+mem%d^uip9&1a*-ql$y+ox^9bgz810#1^o2%l%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-maikonschuma-djangoblog-6wm9t4s5j9u.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = [
+    "8000-maikonschuma-djangoblog-6wm9t4s5j9u.ws.codeinstitute-ide.net",
+    '.herokuapp.com'
+]
 
 
 # Application definition
