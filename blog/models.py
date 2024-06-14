c92e011958baf196bfd29c36e243e3b997f781f1
Maikon Schumann <maikon.schumann@gmail.com>
2024-06-14 16:23:45 +0000

    add comment class

--- blog/models.py
+++ blog/models.py
@@ -19,4 +19,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
